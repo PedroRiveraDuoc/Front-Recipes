030039e7939ee7ae498d4a93930b5f6f62c6201a
gustavoromerocl <gandresrp@gmail.com>
2024-11-10 19:51:24 -0300

    refactor: remove database connection

--- src/main/java/com/example/front_spring_recipes/model/Photo.java
+++ src/main/java/com/example/front_spring_recipes/model/Photo.java
@@ -1,16 +1,12 @@
 package com.example.front_spring_recipes.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 
-@Entity
-@Table(name = "photo")
 public class Photo {
 
   @Id
