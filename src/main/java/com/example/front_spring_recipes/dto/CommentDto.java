90df229d1c17e1275faba758d1e63c90731de111
gustavoromerocl <gandresrp@gmail.com>
2024-12-08 11:14:14 -0300

    feat: add comment service

---
+++ src/main/java/com/example/front_spring_recipes/dto/CommentDto.java
@@ -0,0 +1,15 @@
+package com.example.front_spring_recipes.dto;
+
+public class CommentDto {
+  private String content;
+
+  // Getters y Setters
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+}
